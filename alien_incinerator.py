6ea16fdf0d37798e61b47dbf7b7e07d46217a9e0
yusuf <yusuf_2002@hotmail.co.uk>
2022-08-23 12:04:43 +0100

    Added background image

--- alien_incinerator.py
+++ alien_incinerator.py
@@ -9,18 +9,27 @@ class AlienIncinerator:
         """Initialise game attributes"""
         pygame.init()
         
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.width, self.height = 1200, 800
+        self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Alien Incinerator")
-    
+
+        #self.bg_colour = (82, 139, 167)
+        self.bg_img = pygame.image.load(r"images\background.jpg")
+        self.bg_img = pygame.transform.scale(self.bg_img,(self.width, self.height))
+                
     def run_game(self):
         """Start the game loop"""
+        i = 0
+
         while True:
             # Wait for events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            
             # Update the screen
+             #self.screen.fill(self.bg_colour)
+            self.screen.blit(self.bg_img, (0, 0))
             pygame.display.flip()
             
 if __name__ == "__main__":
